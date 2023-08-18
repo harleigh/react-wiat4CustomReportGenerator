49bad13912c10dc27c84ac3c35c5c90a3d2446eb
harleigh <harleigh.marsh@gmail.com>
2023-08-18 12:45:00 -0700

    moving components up a level

---
+++ src/ReportGenerator.js
@@ -0,0 +1,8 @@
+
+export default function Wiat4ReportGenerator() {
+    return (
+        <>
+        <h1> Wiat-4 Report Generator</h1>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './FileAccessor';  //App is whatever function export default is 
+import App from './ReportGenerator';  //App is whatever function export default is 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
