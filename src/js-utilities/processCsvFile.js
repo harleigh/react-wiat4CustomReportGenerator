20f22def46e26f587a01367a4361379bf72abfdf
harleigh <harleigh.marsh@gmail.com>
2023-08-21 09:10:53 -0700

    added minor error handeling if test does not exist

--- src/js-utilities/processCsvFile.js
+++ src/js-utilities/processCsvFile.js
@@ -5,47 +5,46 @@ import {compositesDict, subTestsDict} from './WIAT-4-Tests'
 export function processCsvFile( fileContents ){
     const data = {}
 
-    //note, csv files have \r for last record in row
+    //note, csv files have "\r" for last record in row
     const rows = fileContents.split('\r\n')
-    console.log(rows)
-/* 
-    const res = rows.find( r => r.includes("Oral Expression"))
-    console.log("Search Result", res)
+    //console.log(rows)
 
-    const [key, score, ssd] = res.split(",")
-    console.log(key, score, ssd)
-    data[key] = [score, ssd]
-    console.log(data)
-  */   
      for(const c in compositesDict) {
         collectEntry(data, rows, c)
-        console.log("Compsite:", c)
+        //console.log("Compsite:", c)
         const allSubtests = compositesDict[c];
-        console.log("   Subtests", allSubtests)
+        //console.log("   Subtests", allSubtests)
          for( const subtest of allSubtests) {
-            console.log("   I am collecting data for ", subtest)
+            //console.log("   I am collecting data for ", subtest)
             collectEntry(data, rows, subtest)
             const allComponents = subTestsDict[subtest]
-            console.log("      Components", allComponents)
+            //console.log("      Components", allComponents)
             for( const component of allComponents) {
-                console.log("      I am collecting data for", component)
+                //console.log("      I am collecting data for", component)
                 collectEntry(data, rows, component)
             } 
         } 
     }
 
     console.log(data)
-    //find(a =>a.includes("#")
 }
 
 function collectEntry(dataDict, dataRows, testName){
-    const [key, score, ssd] = getData(dataRows, testName)
+    let [key, score, ssd] = [NaN, NaN, NaN]
+    try {
+        [key, score, ssd] = getData(dataRows, testName)
+    }catch(e) {
+        console.error(e);
+    }
     dataDict[key] = [score, ssd]
 }
 
 function getData(dataRows, testName){
     const entry = dataRows.find( r => r.includes(testName))
-    console.log(" I tried to find ", testName, "and got", entry )
+    if(entry === undefined) {
+        throw new Error('Critical: I could not find ' + testName);
+    }
+    //console.log(" I tried to find ", testName, "and got", entry )
     const [key, score, ssd] = entry.split(",")
     //console.log("I searched for ", testName)
     //console.log("I Found: ", key, score, ssd)
