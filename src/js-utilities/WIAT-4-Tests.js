ecffb90e72cb07a88e11016bb4c6365e35a42923
harleigh <harleigh.marsh@gmail.com>
2023-08-22 08:34:50 -0700

    moved utility function

--- src/js-utilities/WIAT-4-Tests.js
+++ src/js-utilities/WIAT-4-Tests.js
@@ -72,7 +72,8 @@ export const subTestsDict = {
  *   e.g. IN:  ["Orthographic Fluency", "Spelling", "Orthographic Choice"]
  *        OUT: "Orthographic Fluency, Spelling and Orthographic Choice"
  */
-const getSubtestNames = (list) => {
+/* 
+export const getSubtestNames = (list) => {
     const lastSubtest = list.pop()
     if(list.length > 0) {
         return list.join(", ") + " and " + lastSubtest;
@@ -82,37 +83,8 @@ const getSubtestNames = (list) => {
     }
 }
 
+ */
 
-/**
- *  130 and above Extremely High
-    120 to 130 Very High
-    110 to 120  High Average
-    90-110   Average
-    80 to 90 Low Average
-    70 to 80 Very Low
-    Below 70 Extremely Low
-*/
-const getMeasure = (studentScore) =>{
-    const amt = Number(studentScore)
-    switch(amt) {
-        case ( amt < 70 ):
-            return "Extremely Low"
-        case ( 70<= amt && amt < 80):
-            return "Very Low"
-        case ( 80<= amt && amt < 90):
-            return "Low Average"
-        case ( 90<= amt && amt < 110):
-            return "Average"
-        case ( 110<= amt && amt < 120):
-            return "High Average"
-        case ( 120<= amt && amt < 130):
-            return "Very High"
-        case ( 130<= amt):
-            return "Extremely High"
-        default:
-            return "Score Not Found"
-    }
-}
 
 
 
